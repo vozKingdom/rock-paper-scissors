16c7d9b3998a324528ac7a9d491abd310756739e
vozKingdom <161555246+vozKingdom@users.noreply.github.com>
2024-04-16 07:34:18 +0700

    Refactor comments (practice pseudocode)

--- script.js
+++ script.js
@@ -1,71 +1,43 @@
 
-//const playerSelection = prompt('Rock, Paper, or Scissors?')       
 
-// COMMENT prompt user to input a string "rock", "paper", or "scissors", then store in constant variable playerSelection
-
-// TEST   console.log("User: " + playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase())   
-
-
-function getComputerChoice() {
-    randomComputerChoice = Math.floor(Math.random()*3);
-    if (randomComputerChoice == 0) {
-        return "ROCK"
-    } else if (randomComputerChoice == 1) {
-        return "PAPER"
-    } else if (randomComputerChoice == 2) {
-        return "SCISSOR"
+function getComputerChoice() {  //  Create a function called getComputerChoice that will determine a random choice (rock paper scissor) for the computer.
+    randomComputerChoice = Math.floor(Math.random()*3); // Pick a random  number from 0 to 2, and store it in a variable called randomComputerChoice.
+    if (randomComputerChoice == 0) {    // IF random number is 0 THEN   
+        return "ROCK"   //  RETURN 'ROCK' selection, and END function execution 
+    } else if (randomComputerChoice == 1) { // ELSE random number is 1 
+        return "PAPER"  //  RETURN 'PAPER' selection, and END function execution
+    } else if (randomComputerChoice == 2) { // ELSE random number is 2
+        return "SCISSOR"    //  RETURN 'SCISSOR' selection, and END function execution
     }
 }
 
-//COMMENT create a function called getComputerChoice that will randomnly store the string "rock","paper",or "scissors"
 
+function playRound(playerSelection, computerSelection) {    // Create a function called playRound that will play a single round of "rock paper scissors". the function will take two parameters - the playerSelection and computerSelection.
+    playerSelection = playerSelection.toUpperCase();    //  Convert players selection to uppercase
 
-
-//const computerSelection = getComputerChoice()
-
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase(); 
-
-    if (
+    if (    // IF the computers selection wins THEN
     (computerSelection == "ROCK" && playerSelection == "SCISSOR") ||  
     (computerSelection == "PAPER" && playerSelection == "ROCK") || 
     (computerSelection == "SCISSOR" && playerSelection == "PAPER")) 
-        { return "COMPUTER WIN!";
-    } else if (
+        { return "COMPUTER WIN!";   // RETURN 'COMPUTER WIN' string, and END function execution
+    } else if (     // ELSE IF the players selection wins 
     (playerSelection == "ROCK" && computerSelection == "SCISSOR") ||  
     (playerSelection == "PAPER" && computerSelection == "ROCK") || 
     (playerSelection == "SCISSOR" && computerSelection == "PAPER")) 
-        { return "PLAYER WIN";
-    } else if (
+        { return "PLAYER WIN";     // RETURN 'PLAYER WIN' string, and END function execution
+    } else if (     // ELSE IF the game ends in a tie
     (playerSelection == computerSelection))
-        { return "TIE"
+        { return "TIE"  // RETURN 'TIE' string, and END function execution
     }
-
 } 
 
-// TEST   console.log(playRound(playerSelection, computerSelection))
 
-
-    // COMMENT create a function named playRound that will play a single round of "rock", "paper", or "scissors".
-    // COMMENT the function will take two parameters - the playerSelection and computerSelection.
-    // COMMENT convert playerSelection (user input) to sentence case (rock, ROCK, rOcK = Rock)
-    // COMMENT create if..else statements, of "rock", "paper", or "scissors" combinations.
-    // COMMENT return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
-    // COMMENT Display the result of the round on console.
-
-
-function playGame() {
-    let playerSelection = prompt('ROCK  PAPER  SCISSOR ?') 
-    let computerSelection = getComputerChoice()
-    console.log(playRound(playerSelection, computerSelection));
+function playGame() {   //  Create a function called playGame that will ...
+    let playerSelection = prompt('ROCK  PAPER  SCISSOR ?')  //  OBTAIN the players choice for the game and store in a variable called playerSelection
+    let computerSelection = getComputerChoice()     //  OBTAIN the computers choice and store in a variable called computerSelection   
+    console.log(playRound(playerSelection, computerSelection)); // PRINT result of game onto browser console
     }
 
-
-    //COMMENT create a function named playGame that will: playRound five times, and keeps score and reports a winner or loser at the end.
-
-
-// TEST console.log(playRound(playerSelection, computerSelection));
 
 console.log(playGame());
 console.log(playGame());
